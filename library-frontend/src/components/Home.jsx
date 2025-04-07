b3b913a4b1c58db011b1a281290ebcb2b1aa4789
Inesh-Reddy <ineshreddy007@gmail.com>
2025-04-07 19:15:29 +0530

    Library App : FE : Implemented Listing all Books in Home Page

--- library-frontend/src/components/Home.jsx
+++ library-frontend/src/components/Home.jsx
@@ -1,9 +1,31 @@
+import { useContext } from "react";
+import { LibraryContext } from "../App";
+
 const Home = () => {
-    return(
+  const { books, isLoading, errorMessage } = useContext(LibraryContext);
+
+  return (
+    <div>
+      <h2>Home</h2>
+      {isLoading ? (
+        <h1>{"isLoading..."}</h1>
+      ) : errorMessage ? (
+        <h1>{errorMessage}</h1>
+      ) : (
         <div>
-            <h2>Home</h2>
+          <ol>
+            {books.map((item, index) => {
+              return (
+                <li key={index}>
+                  {item.title}. - {item.author}.
+                </li>
+              );
+            })}
+          </ol>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Home;
